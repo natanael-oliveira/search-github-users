e67a54aa449758fb4572d1e7b6cb4385895fd45d
natanael-oliveira <natanaeloliveiramartinsbr@gmail.com>
2020-10-04 00:27:36 -0300

    manipulando classes - primeiros passos

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-alert('Teste')
-class Teste {
-    metodo(){
 
-    }    
+class List{
+    constructor(){
+        this.data = [];
+    }
+    add(data){
+        this.data.push(data);
+        console.log(data);
+    }
 }
+class TodoList extends List{
+    constructor(){
+        super();
+        this.usuario = "Diego";
+    }
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById("novoTodo").onclick = function(){
+    MinhaLista.add("Novo Todo");
+}
+MinhaLista.mostraUsuario();
