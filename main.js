e0a3146a12eb98d11089d8252554de24ff5e8dfc
natanael-oliveira <natanaeloliveiramartinsbr@gmail.com>
2020-10-05 14:06:13 -0300

    utilizando funções percorrendo uma array

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
+// variaveis const não podem ter seu valor reatribuidos, mas sim mutados
 
-class List{
-    constructor(){
-        this.data = [];
-    }
-    add(data){
-        this.data.push(data);
-        console.log(data);
-    }
-}
-class TodoList extends List{
-    constructor(){
-        super();
-        this.usuario = "Diego";
-    }
-    mostraUsuario(){
-        console.log(this.usuario);
-    }
-}
+//operações com constantes
 
-var MinhaLista = new TodoList();
+//percorre uma array e realiza operações sequenciais dentro dela;
+const arr = [1,3,4,5,8,9,10];
+const newArr = arr.map(function(item,index){
+    return item + index;
+});
+// console.log(newArr);
 
-document.getElementById("novoTodo").onclick = function(){
-    MinhaLista.add("Novo Todo");
-}
-MinhaLista.mostraUsuario();
+//percorre uma array e utiliza o resultado de cada operação na operação futura;   
+const sum = arr.reduce(function(total,next){
+    return total + next;
+});
+// console.log(sum);
+
+//percorre uma array e filtra cada valor que não se encaixa na regra. obs: O retorno precisa ser do tipo boolean 
+const filter = arr.filter(function(item){
+    return item % 2 === 0;
+});
+// console.log(filter);
+
+//buscar dados percorrendo uma aray
+const find = arr.find(function(item){
+    return item === 10;
+});
+// console.log(find);
+
